c37e5818785f2d39a902915d6d114a536a61651f
mohitchaudhry21 <46539652+mohitchaudhry21@users.noreply.github.com>
2026-04-18 19:28:35 +0530

    Update server.py

--- TradingJournal/server.py
+++ TradingJournal/server.py
@@ -52,6 +52,6 @@ def upload_file():
 
 import os
 
-    if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 10000))
-        app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
