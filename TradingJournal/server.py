81f59a2ac71ad9bfe4fccab8620bbf8e99a85e92
mohitchaudhry21 <46539652+mohitchaudhry21@users.noreply.github.com>
2026-04-18 19:16:31 +0530

    Update server.py

--- TradingJournal/server.py
+++ TradingJournal/server.py
@@ -50,5 +50,8 @@ def upload_file():
 
     return jsonify({"status": "success", "trades": data})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 10000))
+        app.run(host="0.0.0.0", port=port)
